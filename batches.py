d686181ae291295961d2ba35a9a3d726aa0b941a
FizzleDorf <1fizzledorf@gmail.com>
2024-06-03 16:43:31 -0400

    remove batches.py

--- batches.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-def process_batch(images, num_frames):
-    image_list = []
-    for i in images:
-        if i != 0:
-            image_list.append(images[i-1], images[i], num_frames[i])
-    return torch.cat(image_list)
